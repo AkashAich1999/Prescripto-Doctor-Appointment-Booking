73a96dea877f201b2a1bbfc584d53778358ce62c
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-12-07 19:32:00 +0530

    TopDoctors Section Created & Also Fixed Some Responsiveness in the Header Section

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,17 +6,17 @@ const Header = () => {
 
         {/* ---------- Left Side ---------- */}
         <div className="md:w-1/2 flex flex-col items-start justify-center gap-4 m-auto py-10 md:py-[10vw] md:mb-[-30px]">
-            <p className="text-3xl md:text-4xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight">
-            Book Appointment <br /> With Trusted Doctors
+            <p className="text-3xl md:text-4xl 2xl:text-5xl text-white font-semibold text-center md:text-left leading-tight md:leading-tight lg:leading-tight xl:leading-tight 2xl:leading-snug">
+            Book Appointment With Trusted Doctors
             </p>
             <div className="flex flex-col md:flex-row gap-3 items-center text-white text-sm font-light">
-                <img className="w-28 " src={assets.group_profiles} alt="" />
-                <p className="">
+                <img className="w-28" src={assets.group_profiles} alt="" />
+                <p className="text-center md:text-left">
                 Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.
                 </p>
             </div>
             
-            <a href="#speciality" className="flex gap-2 items-center bg-white rounded-full px-8 py-3 text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300">
+            <a href="#speciality" className="flex gap-2 items-center bg-white rounded-full px-8 py-3 text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300 outline-none">
             Book Appointement <img src={assets.arrow_icon} alt="" />
             </a>
         </div>
---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,56 @@
+import { useNavigate } from "react-router-dom";
+import { doctors } from "../assets/assets";
+
+const TopDoctors = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex flex-col items-center gap-4 my-16 text-gray-900 md:mx-10">
+        <h1 className="text-3xl font-medium">Top Doctors to Book</h1>
+        <p className="sm:w-1/3 text-center text-sm">Simply browse through our extensive list of trusted doctors.</p>
+        <div className="w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0">
+            {doctors.slice(0, 12).map((item, index) => (
+                <div onClick={() => navigate(`/appointment/${item._id}`)} key={index} className="border border-blue-200 rounded-xl cursor-pointer hover:translate-y-[-10px] transition-all duration-500 overflow-hidden">
+                    <img src={item.image} alt="" className="bg-blue-50" />
+                    <div className="p-4">
+                        <div className="flex items-center gap-2 text-sm text-center text-green-500">
+                            <p className="w-2 h-2 bg-green-500 rounded-full"></p><p>Available</p>
+                        </div>
+                        <p className="text-gray-900 text-lg font-medium">{item.name}</p>
+                        <p className="text-gray-600 text-sm">{item.speciality}</p>
+                    </div>
+                </div>
+            ))}
+        </div>
+        <button className="bg-blue-50 text-gray-600 px-12 py-3 rounded-full mt-10">more</button>
+    </div>
+  )
+}
+
+export default TopDoctors;
+
+{/**
+    We added following in the tailwind.config.js file :
+    
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))'
+      }
+*/}
+
+{/**
+    grid-cols-auto   -->	grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));    
+    
+    1. repeat(auto-fill, …)
+    
+      - The grid fills each row with as many columns as possible
+      - When space is not enough, the next items wrap to the next row
+      - No need to manually specify grid-cols-3, grid-cols-4, etc.
+
+    2. minmax(200px, 1fr)
+
+      - This sets the minimum and maximum width for each column :
+        • Minimum width = 200px
+        • Maximum width = 1fr (fraction of available space)
+      - So each card behaves like:
+        “Be at least 200px wide, but if there is more room, stretch to fill the row.”
+*/}
