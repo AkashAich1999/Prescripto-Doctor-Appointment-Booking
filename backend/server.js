22ac228332f854f09c36e6aff6355c9389e2b109
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2025-12-12 17:11:47 +0530

    Connection Established with Cloudinary and doctorModel & userModel Created

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config"
 import connectDB from "./config/db.js";
+import connectCloudinary from "./config/cloudinary.js";
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+connectCloudinary();
 
 // middlewares
 app.use(express.json());
