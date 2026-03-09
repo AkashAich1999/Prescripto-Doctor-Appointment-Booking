5fbe9b5cadf4e04e4596ba2a195cf2f15ca4049b
AkashAich1999 <108985323+AkashAich1999@users.noreply.github.com>
2026-03-09 20:08:56 +0530

    process.env.PORT

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import doctorRouter from "./routes/doctorRoute.js";
 import userRouter from "./routes/userRoute.js";
 
 const app = express();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 8000;
 
 connectCloudinary();
 
